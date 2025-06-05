38e4ca6558e192a2ef91bca778b3e99c8f841005
mveishu <mveishu@gmail.com>
2025-06-06 03:29:54 +0900

    Update streamlit_litbot.py

--- streamlit_litbot.py
+++ streamlit_litbot.py
@@ -66,7 +66,7 @@ def send_email_with_attachment(file, subject, body, filename):
         smtp.login(st.secrets["email"]["user"], st.secrets["email"]["password"])
         smtp.send_message(msg)
 
-st.title("📚 문학 토론 챗봇 - 리토")
+st.title("📚 문학 토론 챗봇 - 리토: 황순원, <별> 🌟")
 col1, col2 = st.columns(2)
 with col1:
     user_lastname = st.text_input("성을 입력해주세요", key="lastname")
